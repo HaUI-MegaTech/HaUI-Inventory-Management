adde9f4334df633f49b4bf9941445361ee58bb11
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-12-19 14:26:25 +0700

    Add base auth service project

---
+++ auth-service/src/main/java/org/example/authservice/AuthController.java
@@ -0,0 +1,17 @@
+package org.example.authservice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @GetMapping
+    public String test() {
+        return "Hello, auth service!";
+    }
+}
