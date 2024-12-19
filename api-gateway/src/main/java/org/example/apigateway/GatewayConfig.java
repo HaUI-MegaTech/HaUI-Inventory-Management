a23618a8f5501f172c4a8a727fc07f2a99223ed3
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-12-19 10:37:37 +0700

    Add api gateway config

---
+++ api-gateway/src/main/java/org/example/apigateway/GatewayConfig.java
@@ -0,0 +1,32 @@
+package org.example.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(predicateSpec ->
+                        predicateSpec
+                                .path("/api/v1/users/**")
+                                .filters(gatewayFilterSpec ->
+                                        gatewayFilterSpec.addRequestHeader("src", "hello product service....")
+                                )
+                                .uri("http://localhost:8081/")
+                )
+                .route(predicateSpec ->
+                        predicateSpec
+                                .path("/api/v1/products/**")
+                                .filters(gatewayFilterSpec ->
+                                        gatewayFilterSpec.addRequestHeader("src", "hello user service....")
+                                )
+                                .uri("http://localhost:8082/")
+                )
+                .build();
+    }
+}
