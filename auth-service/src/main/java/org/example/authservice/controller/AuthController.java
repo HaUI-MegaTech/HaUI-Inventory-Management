1263bed7acafc3591da2fdae7971eb620c9c5f44
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-12-20 14:02:31 +0700

    Complete add controller methods in auth-service project

--- auth-service/src/main/java/org/example/authservice/controller/AuthController.java
+++ auth-service/src/main/java/org/example/authservice/controller/AuthController.java
@@ -1,17 +1,53 @@
 package org.example.authservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.authservice.dto.auth.AuthenticationRequestDTO;
+import org.example.authservice.dto.auth.RefreshTokenRequestDTO;
+import org.example.authservice.dto.user.AddUserRequestDTO;
+import org.example.authservice.service.AuthenticationService;
+import org.example.authservice.shared.constant.Endpoint;
+import org.example.authservice.shared.global.AuthData;
+import org.example.authservice.shared.global.GlobalResponseDTO;
+import org.example.authservice.shared.global.NoPaginatedMeta;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthenticationService service;
 
-    @GetMapping
-    public String test() {
-        return "Hello, auth service!";
+    @PostMapping(Endpoint.V1.Auth.REGISTER)
+    public ResponseEntity<GlobalResponseDTO<NoPaginatedMeta, AuthData>> register(
+            @RequestBody AddUserRequestDTO request,
+            HttpServletRequest servletRequest
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.register(request, servletRequest));
+    }
+
+    @PostMapping(Endpoint.V1.Auth.AUTHENTICATE)
+    public ResponseEntity<GlobalResponseDTO<NoPaginatedMeta, AuthData>> authenticate(
+            @RequestBody AuthenticationRequestDTO request,
+            HttpServletRequest servletRequest
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.authenticate(request, servletRequest));
+    }
+
+    @PostMapping(Endpoint.V1.Auth.REFRESH)
+    public ResponseEntity<GlobalResponseDTO<NoPaginatedMeta, AuthData>> refresh(
+            @RequestBody RefreshTokenRequestDTO request,
+            HttpServletRequest servletRequest
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.refresh(request, servletRequest));
+    }
+
+    @PostMapping(Endpoint.V1.Auth.LOGOUT)
+    public ResponseEntity<GlobalResponseDTO<NoPaginatedMeta, Void>> logout(
+            @RequestBody RefreshTokenRequestDTO request,
+            HttpServletRequest servletRequest
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.logout(request, servletRequest));
     }
 }
